3ff35da9f82397cb77f163ab357c95d7b8ebe42c
jassielCOTA12 <jassielalt_22@alu.uabcs.mx>
2026-05-22 22:58:40 -0700

    cambio de uri replicacion

--- scripts/crearBaseDeDatos.js
+++ scripts/crearBaseDeDatos.js
@@ -2,7 +2,7 @@
 
 const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb://localhost:27017';
+const uri = 'mongodb://localhost:27017,localhost:27018,localhost:27019/?replicaSet=rsBanco';
 const client = new MongoClient(uri);
 
 async function crearBD() {
